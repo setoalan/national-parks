dc289a38a3e39687cd3238b9c4c609e2457f0e87
Alan Seto <alantseto@gmail.com>
2017-03-20 12:35:58 -0500

    Configure Slick carousel

--- app/controllers/parkController.js
+++ app/controllers/parkController.js
@@ -3,22 +3,24 @@
 angular.module('national-parks')
   .controller('ParkController', function ($scope, $stateParams, parksFactory, parkFactory) {
     $scope.park = parksFactory.getPark($stateParams.id);
+    $scope.isPhotosFetched = false;
     $scope.park.flickrPhotos = [];
 
     console.log($scope.park);
 
-    parkFactory.fetchPhotos($scope.park.fullName.split(' ').join('+'))
-      .then((response) => {
-        response.forEach((photo) => {
-          $scope.park.flickrPhotos.push({
-            'src': `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
-            'alt': photo.title
-          });
-        });
-      });
+    parkFactory.fetchPhotos($scope.park, $scope.park.fullName.split(' ').join('+'))
+      .then(() => $scope.isPhotosFetched = true);
 
-    $('#parkCarousel').carousel({interval: 5000});
-    $('#parkCarousel').carousel('cycle');
+    $scope.slickConfig = {
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      dots: true,
+      draggable: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
 
     $('#parkMap').append(`<iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD1trrr2iGJkn3xWwKZzGoxsQ8pnJLYSrg&q=${$scope.park.fullName}&zoom=9" allowfullscreen</iframe>`);
   });
--- app/services/parkService.js
+++ app/services/parkService.js
@@ -4,10 +4,17 @@ angular.module('national-parks')
   .factory('parkFactory', ($http) => {
     let parkFactory = {};
 
-    parkFactory.fetchPhotos = (parkName) => {
-      let data = $http.get('/park/flickr?parkName=' + parkName)
+    parkFactory.fetchPhotos = (park, parkName) => {
+      const data = $http.get('/park/flickr?parkName=' + parkName)
         .then((response) => {
-          return response.data.body.photos.photo;
+          let photo = response.data.body.photos.photo;
+          photo.forEach((photo) => {
+            park.flickrPhotos.push({
+              'src': `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
+              'alt': photo.title
+            });
+          });
+          return photo;
         }, (error) => {
           console.error('Error: ' + error);
         });
