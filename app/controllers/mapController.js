e964e935bb3f2ab9fecc38f3952aaef000a7d306
Alan Seto <alantseto@gmail.com>
2017-03-27 11:50:57 -0500

    Fix map page

--- app/controllers/mapController.js
+++ app/controllers/mapController.js
@@ -1,20 +1,20 @@
 'use strict';
 
 angular.module('national-parks')
-  .controller('MapController', function ($scope, toolBarFactory, parksFactory) {
+  .controller('MapController', function ($scope, $window, $state, $compile, toolBarFactory, parksFactory) {
+    $('#map').css('height', $window.innerHeight - $('#header').height() - 91);
+
+    const center = new google.maps.LatLng(39.50, -98.35);
+
     const map = new google.maps.Map(document.getElementById('map'), {
       zoom: 3,
-      center: { lat: 26.7135539881, lng: -117.7395580925 },
+      center: center,
       scrollwheel: false
     });
 
-    const addMapListeners = (marker, infoWindow) => {
-      marker.addListener('click', () => {
-        infoWindow.open(map, marker);
-      });
-      google.maps.event.addListener(map, 'click', () => {
-        infoWindow.close();
-      });
+    const resize = () => {
+      map.setCenter(center);
+      $('#map').css('height', $window.innerHeight - $('#header').height() - 91);
     };
 
     const userLocation = toolBarFactory.getUserLocation();
@@ -23,11 +23,16 @@ angular.module('national-parks')
         position: userLocation,
         map: map
       });
+
       userMarker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+
       const infoWindow = new google.maps.InfoWindow({
         content: '<h5>Your Location</h5>'
       });
-      addMapListeners(userMarker, infoWindow);
+
+      userMarker.addListener('click', () => {
+        infoWindow.open(map, userMarker);
+      });
     }
 
     parksFactory.getParks().forEach((park) => {
@@ -35,10 +40,29 @@ angular.module('national-parks')
         position: { lat: park.latLong.lat, lng: park.latLong.lng },
         map: map
       });
-      const infoWindow = new google.maps.InfoWindow({
-        content: `<h5>${park.fullName}</h5>` +
-          `<h6>${park.foursquare.venue.location.city}, ${park.foursquare.venue.location.cc}</h6>`
-      });
-      addMapListeners(marker, infoWindow);
+
+      var infoContent =
+        `<div>
+          <h4><a ng-click="goToPark('${park.parkCode}')">${park.fullName}</a></h4>
+          <p>${park.states}</p>
+        </div>`;
+      var compiledContent = $compile(infoContent)($scope);
+
+      google.maps.event.addListener(marker, 'click', ((marker, content, scope) => {
+        return () => {
+          scope.infowindow.setContent(content);
+          scope.infowindow.open(scope.map, marker);
+        };
+      })(marker, compiledContent[0], $scope));
     });
+
+    $scope.infowindow = new google.maps.InfoWindow({
+      content: ''
+    });
+
+    $scope.goToPark = (parkCode) => {
+      $state.go(`app.park`, {id: parkCode});
+    };
+
+    google.maps.event.addDomListener(window, 'resize', resize);
   });
